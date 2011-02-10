751a613e4e9d7d76bf5f53a9278fa81d86405e51
Brian <brian@insightify.com>
2011-02-09 20:40:06 -0800

    initial commit

---
+++ config/deploy.rb
@@ -0,0 +1,13 @@
+set :repository,  "git@github.com:syweb/koc13672.org.git"
+set :user,        "sites"
+set :domain,      "#{user}@insightmethods.com"
+set :deploy_to,   "/home/sites/koc13672.org"
+set :port,          4000
+set :merb_env,      "preview"
+set :processes,     1
+
+
+
+namespace :vlad do
+
+end
